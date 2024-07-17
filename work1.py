6e62e504028b929fc0d304a11a28b7110cf929e3
TigranBagaDZHYAN <127658552+TigranBagaDZHYAN@users.noreply.github.com>
2024-07-17 22:27:59 +0300

    Add files via upload

---
+++ work1.py
@@ -0,0 +1,9 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(6.67>6.66 and 4444 != 4444.8)
+# 3rd program
+print(1234 % 1000 // 10 + 5678 % 1000 // 10)
+# 4th program
+print(int(25.50)==int(50.25//2)) # 1 ver.
+## print(int(25.50)*2==int(50.25)) # 2 var.
