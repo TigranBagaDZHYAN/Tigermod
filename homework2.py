a7702cdb7469f519c03cd8106c8964956890d2d9
Tigran <127658552+TigranBagaDZHYAN@users.noreply.github.com>
2024-07-18 22:16:58 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_tasks=12
+hours_spent=1.5
+course_name='Python'
+time_for_one_task=hours_spent/completed_tasks
+print('Курс:',course_name,'Всего задач:',completed_tasks,'Затрачено часов:',hours_spent,'Среднее время выполнения задания:',time_for_one_task,'часа')
