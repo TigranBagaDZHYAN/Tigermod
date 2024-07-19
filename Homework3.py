a58b483ceabadab48cdc7d795d92dc0c40bd28d6
Tigran <127658552+TigranBagaDZHYAN@users.noreply.github.com>
2024-07-19 08:40:32 +0300

    Update Homework3.py

--- Homework3.py
+++ Homework3.py
@@ -2,7 +2,7 @@ name='Tigran'
 print('Name:',name)
 age=25
 print('Age:',age)
-new_age=30
-print('New age:',new_age)
+age=age+5
+print('New age:',age)
 is_student=True
 print('Is_student:',is_student)
