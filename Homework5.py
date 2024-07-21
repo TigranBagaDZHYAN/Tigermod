8ca55a42ed5a9cb8bca4d0437d12865a6452cac2
Tigran <127658552+TigranBagaDZHYAN@users.noreply.github.com>
2024-07-21 11:33:31 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,9 @@
+immutable_var=12,1,99,'Tigran',True
+print(immutable_var)
+#immutable_var[2]=98 #Тип tuble неизменяемый
+#print(immutable_var) #Выдает ошибку
+  #Ответ:Попробовал изменить, выводит ошибку так как в уроке было уже сказано что "кортежи" неизменяемые списки
+
+mutable_list = ['В этом списке','могу менять эелементы','потому что он ','        ']
+mutable_list[3]='в квадратных скобках'
+print(mutable_list)
