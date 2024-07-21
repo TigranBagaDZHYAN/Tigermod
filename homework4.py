0d33ad3fd23dc40d925fcae1815742361ce9fb85
Tigran <127658552+TigranBagaDZHYAN@users.noreply.github.com>
2024-07-21 10:48:26 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+#1 часть
+my_string=input('Еnter the word: ')#Создайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input()).
+print('Number of characters: ',len(my_string))#Вывести количество символов введённого текста
+#2 часть
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
