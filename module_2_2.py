1bdf56e59a8615d44026d72703dafd6daed053bf
Tigran <127658552+TigranBagaDZHYAN@users.noreply.github.com>
2024-07-24 17:35:36 +0300

    module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,10 +1,10 @@
-first =int(input('Ввведите первое число- '))
-second=int(input('Введите второе число- '))
-third=int(input('Введите третье число- '))
+first = int(input('Ввведите первое число- '))
+second = int(input('Введите второе число- '))
+third = int(input('Введите третье число- '))
 
-if first==second==third:
+if first == second == third:
     print('3')
-elif first==second or second==third or third==first:
+elif first == second or second == third or third == first:
     print('2')
 else:
     print('0')
