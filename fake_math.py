da1a42e3c8252b6d5f0a26c8ead11d9cb9ab4f28
Tigran <127658552+TigranBagaDZHYAN@users.noreply.github.com>
2024-08-10 19:28:39 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+
+
+def divide(first,second):
+    if second == 0:
+        return "Ошибка"
+    else:
+        return first / second
+
+
---
+++ true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+
+def divide(first, second):
+    if second != 0:
+        f = first / second
+        return f
+    else:
+        return inf
+
+
