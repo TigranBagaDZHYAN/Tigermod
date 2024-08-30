ab45636e07ea477fecbf2ff55fd0d192d0dba8a4
Tigran <127658552+TigranBagaDZHYAN@users.noreply.github.com>
2024-08-30 21:16:02 +0300

    Add files via upload

---
+++ module5.3.py
@@ -0,0 +1,96 @@
+class House:
+    def __init__(self,name,number_of_flors):
+        self.name = name
+        self.number_of_floors = number_of_flors
+
+
+
+    def go_to(self,new_floor):
+         if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1,new_floor + 1):
+                print(floor)
+         else:
+            print("\nТакого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+    def __str__(self):
+        return f'Название: "{self.name}", кол-во этажей: {self.number_of_floors}.'
+
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+
+    def __lt__(self, other):
+    # (<)
+        return self.number_of_floors < other.number_of_floors
+
+
+    def __le__(self, other):
+    # (<=)
+        return self.number_of_floors <= other.number_of_floors
+
+
+    def __gt__(self, other):
+    # (>)
+        return self.number_of_floors > other.number_of_floors
+
+
+    def __ge__(self, other):
+    # (>=)
+        return self.number_of_floors >= other.number_of_floors
+
+
+    def __ne__(self, other):
+    # (!=)
+        return self.number_of_floors != other.number_of_floors
+
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+
+    def __iadd__(self, other):
+        return self + other
+
+
+    def __radd__(self, other):
+        return self + other
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+def main():
+    h1 = House('ЖК Эльбрус', 10)
+    h2 = House('ЖК Акация', 20)
+
+    print(h1)
+    print(h2)
+
+    print(h1 == h2)  # __eq__
+
+    h1 = h1 + 10  # __add__
+    print(h1)
+    print(h1 == h2)
+
+    h1 += 10  # __iadd__
+    print(h1)
+
+    h2 = 10 + h2  # __radd__
+    print(h2)
+
+    print(h1 > h2)  # __gt__
+    print(h1 >= h2)  # __ge__
+    print(h1 < h2)  # __lt__
+    print(h1 <= h2)  # __le__
+    print(h1 != h2)  # __ne__
+
+
+if __name__ == '__main__':
+    main()
+
