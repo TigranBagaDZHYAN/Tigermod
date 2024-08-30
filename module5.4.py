a8d673d2171a69d7a4ee98511396da6dcacbbd97
Tigran <127658552+TigranBagaDZHYAN@users.noreply.github.com>
2024-08-30 21:25:04 +0300

    Add files via upload

---
+++ module5.4.py
@@ -0,0 +1,31 @@
+class House:
+
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return self.name
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+
+del h2
+del h3
+
+print(House.houses_history)
